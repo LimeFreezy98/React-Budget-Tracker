51bfe9135cb6ca616a99bbf8877bcdada395362d
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-11-14 07:55:38 -0700

    added display income expense

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 
 import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
     <GlobalProvider>
         <div className='container'>
           <h2 className= "mb-4">Expance/Budget Tracker</h2>
-        
+          <Balance />
 
+          <IncomeExpense />
+          
         </div>
         </GlobalProvider>
 
